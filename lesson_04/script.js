0afbaca6ec0711e16f264b1ae8958febd0b00792
Kaminsky <kaminskyfrontend@gmail.com>
2021-11-24 12:22:46 +0200

    lesson_04/basic

---
+++ lesson_04/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// Первая задача:
+// В прошлом задании вы от пользователя получали количество товара и цену
+// ✔ С помощью конструкции if (else) убедитесь что пользователь ввёл число
+// ✔ Если число, то всё должно работать как и в прошлом задании
+// ✔ Если пользователь вёл не число, то вывести сообщение "Вы ввели не корректные данные"
+// ✔ Других сообщений в консоли быть не должно
+
+{
+    const productTitle = prompt("Наименование товара");
+    let productCategory = "";
+    let totalAmount = 0;
+
+    let productCount = Number(prompt("Количество товара"));
+
+    if (Number.isNaN(productCount)) {
+        alert("Вы ввели не корректные данные! Должно быть число!");
+        productCount = Number(prompt("Количество товара"));
+    } else {
+        productCategory = prompt("Категория товара");
+    }
+
+    productCategory = prompt("Категория товара");
+
+    let productPrice = Number(prompt("Цена товара"));
+
+    if (Number.isNaN(productPrice)) {
+        alert("Вы ввели не корректные данные! Должно быть число!");
+        productPrice = Number(prompt("Цена товара"));
+    }
+    totalAmount = productPrice * productCount;
+
+    console.log(`
+        Нименование товара: ${productTitle}
+        Количество товара: ${productCount}
+        Категория товара: ${productCategory}
+        Общая сумма: ${totalAmount} RUB
+    `);
+}
