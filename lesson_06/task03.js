cfa079a5144f3269bf80a6a63129409e130640c2
Kaminsky <kaminskyfrontend@gmail.com>
2021-11-30 22:04:19 +0200

    str

--- lesson_06/task03.js
+++ lesson_06/task03.js
@@ -6,8 +6,10 @@
 // Пример: "Цикл" => "лкиЦ"
 
 const reversStr = (str) => {
+    let newStr = ""
     for (let i = str.length - 1; i >= 0; i--) {
-        console.log(str[i]);
+        newStr += str[i]
     }
+    return console.log(newStr);
 };
 reversStr("World");
