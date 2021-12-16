0ae37f547fd4c7855ff70c3650936496bb9cd342
Kaminsky <kaminskyfrontend@gmail.com>
2021-12-17 01:24:20 +0200

    english version

--- RPS_game/game.js
+++ RPS_game/game.js
@@ -3,8 +3,8 @@
 (() => {
   const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
   const FIGURES_ENG = ['stone', 'scissors', 'paper'];
-  let userFigureNum = 0
-  let botFigureNum = 0
+  let userFigureNum = 0;
+  let botFigureNum = 0;
   let totalGameCount = 0;
   let userWinCount = 0;
   let botWinCount = 0;
@@ -13,186 +13,231 @@
   // * рандомный ход бота
   const getBotRandomFigure = (arr) => {
     const random = Math.floor(Math.random() * arr.length);
-    if(arr[random] === 'камень' || arr[random] === 'stone') {
-      botFigureNum = 1
+    if (arr[random] === 'камень' || arr[random] === 'stone') {
+      botFigureNum = 1;
     }
-    if(arr[random] === 'ножницы' || arr[random] === 'scissors') {
-      botFigureNum = 2
+    if (arr[random] === 'ножницы' || arr[random] === 'scissors') {
+      botFigureNum = 2;
     }
-    if(arr[random] === 'бумага' || arr[random] === 'paper') {
-      botFigureNum = 3
+    if (arr[random] === 'бумага' || arr[random] === 'paper') {
+      botFigureNum = 3;
     }
     return arr[random];
   };
 
-  const botFigure = getBotRandomFigure(FIGURES_RUS);
-  console.log('botFigure: ', botFigure);
-
   // * выбор игрока
   const getUserFigure = (lang = 'RUS') => {
-    let choice = prompt('Что вы выбрали?');
+    if (lang === 'RUS') {
+      let choice = prompt('Что вы выбрали?');
 
-    if (choice === null) {
-      const exit = confirm('Вы уверены что не хотите играть?');
+      if (choice === null) {
+        const exit = confirm('Вы уверены что не хотите играть?');
 
-      if (exit) {
-        alert('Ok');
-        alert(`Результат игр: 
+        if (exit) {
+          alert('Ok');
+          alert(`Результат игр: 
                 Сыгранно игр: ${totalGameCount}
                 Побед Бота: ${botWinCount}
                 Побед Игрока: ${userWinCount}
                 Ничьих: ${drawCount}
             `);
-      } else {
-        getGame(lang);
+        } else {
+          getGame(lang);
+        }
       }
-    }
 
-    // !!! если нажали отменить
+      // !!! если нажали отменить
 
-    if (choice != null) {
-      choice.trim().toLowerCase();
+      if (choice != null) {
+        choice.trim().toLowerCase();
 
-      if (choice[0] === 'н') {
-        choice = 'ножницы'
-        userFigureNum = 2;
-      };
-      if (choice[0] === 'к') {
-        choice = 'камень'
-        userFigureNum = 1
-      };
-      if (choice[0] === 'б') {
-        choice = 'бумага'
-        userFigureNum = 3
-      };
+        if (choice[0] === 'н') {
+          choice = 'ножницы';
+          userFigureNum = 2;
+        }
+        if (choice[0] === 'к') {
+          choice = 'камень';
+          userFigureNum = 1;
+        }
+        if (choice[0] === 'б') {
+          choice = 'бумага';
+          userFigureNum = 3;
+        }
 
-      // * неверный ввод
-      if (choice[0] != 'н' && choice[0] != 'б' && choice[0] != 'к') {
-        alert(`
+        // * неверный ввод
+        if (choice[0] != 'н' && choice[0] != 'б' && choice[0] != 'к') {
+          alert(`
         Неверный ввод! 
         Вы ввели ${choice},
         а нужно: 'камень' или 'ножницы' или 'бумага'`);
-        getGame(lang);
+          getGame(lang);
+        }
+        return choice;
       }
-      return choice;
     }
 
     // !!! English
-    // if (lang === 'ENG') {
-    //   let choice = prompt('What your choice?');
+    if (lang === 'ENG') {
+      let choice = prompt('What your choice?');
 
-    //   if (choice === null) {
-    //     const exit = confirm(`Are you sure you don't want to play?`);
-  
-    //     if (exit) {
-    //       alert('Ok');
-    //       alert(`Games result: 
-    //               Games played: ${totalGameCount}
-    //               Bot's wings: ${botWinCount}
-    //               User's wings: ${userWinCount}
-    //               Draws: ${drawCount}
-    //           `);
-    //     } else {
-    //       getGame(lang);
-    //     }
-    //   }
-  
-    //   // !!! если нажали отменить
-  
-    //   if (choice != null) {
-    //     choice.trim().toLowerCase();
-  
-    //     if (choice[0] === 's' && choice[1] === 'c') {
-    //       choice = 'scissors'
-    //       userFigureNum = 2;
-    //     };
-    //     if (choice[0] === 's' && choice[1] === 't') {
-    //       choice = 'stone'
-    //       userFigureNum = 1
-    //     };
-    //     if (choice[0] === 'p') {
-    //       choice = 'paper'
-    //       userFigureNum = 3
-    //     };
-  
-    //     // * неверный ввод
-    //     if (choice[0] != 's' && choice[0] != 'p' && choice[0] != 's') {
-    //       alert(`
-    //       Invalid input! 
-    //       You entered: ${choice},
-    //       But you need: 'stone' or 'scissors' or 'paper'`);
-    //       getGame(lang);
-    //     }
-    //     return choice;
-    //   }
-    // }
+      if (choice === null) {
+        const exit = confirm(`Are you sure you don't want to play?`);
+
+        if (exit) {
+          alert('Ok');
+          alert(`Games result: 
+                  Games played: ${totalGameCount}
+                  Bot's wings: ${botWinCount}
+                  User's wings: ${userWinCount}
+                  Draws: ${drawCount}
+              `);
+        } else {
+          getGame(lang);
+        }
+      }
+
+      // !!! is EXIT
+
+      if (choice != null) {
+        choice.trim().toLowerCase();
+
+        if (choice[0] === 's' && choice[1] === 'c') {
+          choice = 'scissors';
+          userFigureNum = 2;
+        }
+        if (choice[0] === 's' && choice[1] === 't') {
+          choice = 'stone';
+          userFigureNum = 1;
+        }
+        if (choice[0] === 'p') {
+          choice = 'paper';
+          userFigureNum = 3;
+        }
+
+        // * invalid input
+        if (choice[0] != 's' && choice[0] != 'p' && choice[0] != 's') {
+          alert(`
+          Invalid input! 
+          You entered: ${choice},
+          But you need: 'stone' or 'scissors' or 'paper'`);
+          getGame(lang);
+        }
+        return choice;
+      }
+    }
   };
 
   // * игра
   const getGame = (lang = 'RUS') => {
-    console.log('lang: ', lang);
     totalGameCount++;
 
+    let botFigure = '';
+
+    if (lang === 'RUS') {
+      botFigure = getBotRandomFigure(FIGURES_RUS);
+    }
+    if (lang === 'ENG') {
+      botFigure = getBotRandomFigure(FIGURES_ENG);
+    }
+
     const userFigure = getUserFigure(lang);
-    console.log('lang: ', lang);
-// 1 камень 2 ножницы 3 бумага
+    // 1 камень 2 ножницы 3 бумага
     // * условие победы и увеличение счетчиков
-    if(userFigureNum === 1 && botFigureNum === 2 || userFigureNum === 2 && botFigureNum === 3) {
-      userWinCount++;
-      alert(`
-        Бот: ${botFigure}
-        Игрок: ${userFigure}
-        Игрок Победил!
-        `);
-    }
-    if(userFigureNum === 3 && botFigureNum === 2 || userFigureNum === 1 && botFigureNum === 3) {
-      botWinCount++;
-      alert(`
-        Бот: ${botFigure}
-        Игрок: ${userFigure}
-        Бот Победил!
-        `);
-    }
-    if (userFigureNum === botFigureNum) {
-      drawCount++;
-      alert(`
-        Бот: ${botFigure}
-        Игрок: ${userFigure}
-        Ничья!
-        `);
-    }
+    if (lang === 'RUS') {
+      if (
+        (userFigureNum === 1 && botFigureNum === 2) ||
+        (userFigureNum === 2 && botFigureNum === 3)
+      ) {
+        userWinCount++;
+        alert(`
+          Бот: ${botFigure}
+          Игрок: ${userFigure}
+          Игрок Победил!
+          `);
+      }
+      if (
+        (userFigureNum === 3 && botFigureNum === 2) ||
+        (userFigureNum === 1 && botFigureNum === 3)
+      ) {
+        botWinCount++;
+        alert(`
+          Бот: ${botFigure}
+          Игрок: ${userFigure}
+          Бот Победил!
+          `);
+      }
+      if (userFigureNum === botFigureNum) {
+        drawCount++;
+        alert(`
+          Бот: ${botFigure}
+          Игрок: ${userFigure}
+          Ничья!
+          `);
+      }
 
-    // * выход из игры или еще партия
+      // * выход из игры или еще партия
 
-    const nextGame = confirm('Еще разок?');
-    if (nextGame) {
-      getGame(lang);
-    } else {
-      alert(`Результат игр: 
-                  Сыгранно игр: ${totalGameCount}
-                  Побед Бота: ${botWinCount}
-                  Побед Игрока: ${userWinCount}
-                  Ничьих: ${drawCount}
-              `);
-      totalGameCount = botWinCount = userWinCount = drawCount = 0;
+      const nextGame = confirm('Еще разок?');
+      if (nextGame) {
+        getGame(lang);
+      } else {
+        alert(`Результат игр: 
+                    Сыгранно игр: ${totalGameCount}
+                    Побед Бота: ${botWinCount}
+                    Побед Игрока: ${userWinCount}
+                    Ничьих: ${drawCount}
+                `);
+        totalGameCount = botWinCount = userWinCount = drawCount = 0;
+      }
     }
 
     // !!! English
-    // if (lang === 'ENG') {
-    //   const nextGame = confirm('One more time?');
-    // if (nextGame) {
-    //   getGame(lang);
-    // } else {
-    //   alert(`Games result: 
-    //               Games played: ${totalGameCount}
-    //               Bot's wings: ${botWinCount}
-    //               User's wings: ${userWinCount}
-    //               Draws: ${drawCount}
-    //           `);
-    //   totalGameCount = botWinCount = userWinCount = drawCount = 0;
-    // }
-    // }
-    
+    if (lang === 'ENG') {
+      if (
+        (userFigureNum === 1 && botFigureNum === 2) ||
+        (userFigureNum === 2 && botFigureNum === 3)
+      ) {
+        userWinCount++;
+        alert(`
+            Bot: ${botFigure}
+            User: ${userFigure}
+            User Win!
+            `);
+      }
+      if (
+        (userFigureNum === 3 && botFigureNum === 2) ||
+        (userFigureNum === 1 && botFigureNum === 3)
+      ) {
+        botWinCount++;
+        alert(`
+            Bot: ${botFigure}
+            User: ${userFigure}
+            Bot Win!
+            `);
+      }
+      if (userFigureNum === botFigureNum) {
+        drawCount++;
+        alert(`
+            Bot: ${botFigure}
+            User: ${userFigure}
+            Draw!
+            `);
+      }
+
+      // * Exit game or New game
+      const nextGame = confirm('One more time?');
+      if (nextGame) {
+        getGame(lang);
+      } else {
+        alert(`Games result: 
+                  Games played: ${totalGameCount}
+                  Bot's wings: ${botWinCount}
+                  User's wings: ${userWinCount}
+                  Draws: ${drawCount}
+              `);
+        totalGameCount = botWinCount = userWinCount = drawCount = 0;
+      }
+    }
   };
 
   window.RPS = getGame;
--- RPS_game/main.js
+++ RPS_game/main.js
@@ -1,21 +1,18 @@
 'use strict';
 
-const startGame = window.RPS();
-// console.log('startGame: ', startGame);
-startGame()
+(() => {
+  const langChoice = prompt('Выберети язык игры. RUS или ENG')
+    .trim()
+    .toUpperCase();
+  let startGame = null;
 
-// const getLangChoiceAndStartGame = () => {
-//   const langChoice = prompt('Выберети язык игры. RUS или ENG').trim().toUpperCase();
-//   let startGame = null;
+  if (langChoice[0] === 'R') {
+    startGame = window.RPS('RUS');
+  }
 
-//   if (langChoice[0] === 'R') {
-//     startGame = window.RPS('RUS');
-//   }
+  if (langChoice[0] === 'E') {
+    startGame = window.RPS('ENG');
+  }
 
-//   if (langChoice[0] === 'E') {
-//     startGame = window.RPS('ENG');
-//   }
-
-//   startGame();
-// };
-// getLangChoiceAndStartGame()
+  startGame();
+})();
