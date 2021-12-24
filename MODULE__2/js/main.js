1c6128199044159d08cb3532b5c70484572b843d
Kaminsky <kaminskyfrontend@gmail.com>
2021-12-24 10:42:40 +0200

    module_2

---
+++ MODULE__2/js/main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// 1) уберем рекламму
+// 2) упорядочим порядок картинок
+// 3) переместим содержимое списков согласно правильному варрианту
+
+// 1) уберем рекламму
+const ads = document.querySelector('.ads');
+ads.remove();
+
+// 2) упорядочим порядок картинок
+
+const itemsList = document.querySelector('.items');
+const items = itemsList.querySelectorAll('.item');
+
+// порядок картинок
+itemsList.prepend(items[1]);
+items[2].after(items[0]);
+items[0].before(items[3]);
+
+// порядок содержимого списков
+const propsList = document.querySelectorAll('.props__list');
+
+const propsItem = [];
+
+propsList.forEach((item) => {
+  const li = item.querySelectorAll('.props__item');
+  propsItem.push(li);
+  return propsItem;
+});
+
+propsItem[3][3].after(propsItem[1][3]);
+propsItem[1][8].after(propsItem[5][8]);
+propsItem[1][8].after(propsItem[5][9]);
+
+const thirdItemList = propsList[2].cloneNode(true)
+const fourthItemList = propsList[4].cloneNode(true)
+
+propsList[4].replaceWith(thirdItemList)
+propsList[2].replaceWith(fourthItemList)
